d1b5c2dd33af0249107ec2cca6894d85fde6227a
SongShuhang <java0120@qq.com>
2020-07-22 17:42:41 +0800

    Spring MVC RESTful

--- src/main/java/com/jsu/controller/portal/ProductController.java
+++ src/main/java/com/jsu/controller/portal/ProductController.java
@@ -4,11 +4,10 @@ import com.github.pagehelper.PageInfo;
 import com.jsu.common.ServerResponse;
 import com.jsu.service.IProductService;
 import com.jsu.vo.ProductDetailVo;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 //前端用户商品页面控制
 @Controller
@@ -31,6 +30,16 @@ public class ProductController {
     }
 
     /**
+     * 将detail接口改造为RESTful访问（资源定位）
+     * 资源定位的uri地址：http://www.guangping.store/product/19（测试）
+     */
+    @RequestMapping(value = "/{productId}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<ProductDetailVo> detailRESTful(@PathVariable Integer productId){
+        return iProductService.getProductDetail(productId);
+    }
+
+    /**
      * 前端用户关键字搜索控制器
      * @param keyword 关键字
      * @param categoryId 分类id
@@ -47,5 +56,75 @@ public class ProductController {
                                          @RequestParam(value = "pageSize",defaultValue = "10") int pageSize,
                                          @RequestParam(value = "orderBy",defaultValue = "") String orderBy){
         return iProductService.getProductByKeywordCategory(keyword, categoryId, pageNum, pageSize, orderBy);
+    }
+
+    /**
+     * 将list接口改造为RESTful访问（资源定位）
+     * 同时传递keyword和categoryId属性
+     * 资源定位的uri地址：http://www.guangping.store/product/手机/10007/1/10/price_asc（测试）
+     */
+    @RequestMapping(value = "/{keyword}/{categoryId}/{pageNum}/{pageSize}/{orderBy}", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTful(@PathVariable(value = "keyword") String keyword,
+                                         @PathVariable(value = "categoryId")Integer categoryId,
+                                         @PathVariable(value = "pageNum") Integer pageNum,
+                                         @PathVariable(value = "pageSize") Integer pageSize,
+                                         @PathVariable(value = "orderBy") String orderBy){
+        if(pageNum == null){
+            pageNum = 1;
+        }
+        if(pageSize == null){
+            pageSize = 10;
+        }
+        if(StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+        return iProductService.getProductByKeywordCategory(keyword, categoryId, pageNum, pageSize, orderBy);
+    }
+
+    /**
+     * 将list接口改造为RESTful访问（通过keyword的前缀来进行资源定位）
+     * 只传递keyword属性
+     * 资源定位的uri地址：http://www.happymmall.com/product/keyword/手机/1/10/price_asc（测试）
+     */
+    @RequestMapping(value = "/keyword/{keyword}/{pageNum}/{pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTful(@PathVariable(value = "keyword")String keyword,
+                                                @PathVariable(value = "pageNum") Integer pageNum,
+                                                @PathVariable(value = "pageSize") Integer pageSize,
+                                                @PathVariable(value = "orderBy") String orderBy){
+        if(pageNum == null){
+            pageNum = 1;
+        }
+        if(pageSize == null){
+            pageSize = 10;
+        }
+        if(StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+        return iProductService.getProductByKeywordCategory(keyword,null,pageNum,pageSize,orderBy);
+    }
+
+    /**
+     * 将list接口改造为RESTful访问（通过category的前缀来进行资源定位）
+     * 只传递categoryId属性
+     * 资源定位的uri地址：http://www.happymmall.com/product/category/10007/1/10/price_asc（测试）
+     */
+    @RequestMapping(value = "/category/{categoryId}/{pageNum}/{pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTful(@PathVariable(value = "categoryId")Integer categoryId,
+                                                @PathVariable(value = "pageNum") Integer pageNum,
+                                                @PathVariable(value = "pageSize") Integer pageSize,
+                                                @PathVariable(value = "orderBy") String orderBy){
+        if(pageNum == null){
+            pageNum = 1;
+        }
+        if(pageSize == null){
+            pageSize = 10;
+        }
+        if(StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+        return iProductService.getProductByKeywordCategory("",categoryId,pageNum,pageSize,orderBy);
     }
 }

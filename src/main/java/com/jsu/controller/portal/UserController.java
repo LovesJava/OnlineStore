41ebc817b2641571d30688cbf83987a65cf0b3cd
SongShuhang <java0120@qq.com>
2020-08-11 18:21:56 +0800

    login bug

--- src/main/java/com/jsu/controller/portal/UserController.java
+++ src/main/java/com/jsu/controller/portal/UserController.java
@@ -107,8 +107,6 @@ public class UserController {
         //获取loginToken对应的值
         String loginToken = CookieUtil.readLoginToken(httpServletRequest);
         if (StringUtils.isEmpty(loginToken)){
-            //2020.08.11修改一处
-            //return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录，无法获取当前用户的信息");
             return ServerResponse.createByErrorMessage("用户未登录，无法获取当前用户的信息");
         }
         //通过loginToken从Redis中获取user序列化后的字符串
@@ -119,7 +117,7 @@ public class UserController {
         if (user != null){
             return ServerResponse.createBySuccess(user);
         }
-        return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录，无法获取当前用户的信息");
+        return ServerResponse.createByErrorMessage("用户未登录,无法获取当前用户的信息");
     }
 
     /**
